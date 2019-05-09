fa573e1d7bf68c83d28bc213f535a83baa8ffb09
okn-yu <sample@mail.com>
2019-05-09 18:02:36 +0900

    updated.

--- src/loss_function.py
+++ src/loss_function.py
@@ -13,7 +13,8 @@ def mean_square_error(y, t):
 def cross_entropy_error(y, t):
     delta = 1e-7
     loss =  -np.sum(t * np.log(y + delta))
-    #print("loss...%s" % loss)
+    print("cross_entropy_error")
+    print("loss...%s" % loss)
     return loss
 
 
--- src/train_neuralnet.py
+++ src/train_neuralnet.py
@@ -31,8 +31,8 @@ for i in range(ITERS_NUM):
     for key in ('W1', 'b1', 'W2', 'b2'):
         network.params[key] -= LEARNING_RATE * grad[key]
 
-    loss = network.loss(x_batch, t_batch)
-    train_loss_list.append(loss)
+    #loss = network.loss(x_batch, t_batch)
+    #train_loss_list.append(loss)
 
     if i % ITER_PER_EPOC == 0:
         train_acc = network.accuracy(x_train, t_train)
