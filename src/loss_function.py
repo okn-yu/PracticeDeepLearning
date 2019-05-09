711e77e519d1a1f868d3bf9c2312fd1fd7409e49
okn-yu <sample@mail.com>
2019-05-09 18:26:02 +0900

    updated.

--- src/loss_function.py
+++ src/loss_function.py
@@ -16,7 +16,7 @@ def cross_entropy_error(y, t):
 
     delta = 1e-7
     loss =  -np.sum(t * np.log(y + delta))
-    print("cross_entropy_error")
+    print("master")
     print("loss...%s" % loss)
     return loss
 
