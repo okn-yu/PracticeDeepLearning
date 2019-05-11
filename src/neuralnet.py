2429d07a9d814aba60f3d4c2cd63723b6a204b7a
okn-yu <mofuneko@okn-yunoMacBook-Air.local>
2019-05-11 14:26:57 +0900

    updated.

--- src/neuralnet.py
+++ src/neuralnet.py
@@ -4,30 +4,28 @@ from src.layer import AffineLayer, SoftmaxWithLossLayer, ReluLayer
 
 
 class NeuralNet():
-
     def __init__(self, input_size, hidden_size, output_size):
-
         self.layers = []
         self.layers.append(AffineLayer(hidden_size, input_size))
         self.layers.append(ReluLayer())
         self.layers.append(AffineLayer(output_size, hidden_size))
         self.layers.append(SoftmaxWithLossLayer())
 
-    def predict(self, x):
-
+    def _forward(self, x):
         for layer in self.layers:
             x = layer.forward(x)
 
         return x
 
-
-    def loss(self, x, t):
-        y = self.predict(x)
-
+    def _loss(self, t):
         return self.layers[3].loss(t)
 
+    def _backkward(self, dout):
+        for layer in reversed(self.layers):
+            dout = layer.backward(dout)
+
     def accuracy(self, x, t):
-        y = self.predict(x)
+        y = self._forward(x)
         y = np.argmax(y, axis=0)
         t = np.argmax(t, axis=0)
 
@@ -37,13 +35,11 @@ class NeuralNet():
 
     def train(self, x, t):
 
-        self.loss(x, t)
+        self._forward(x)
+        self._loss(t)
+        self._backkward(dout=1)
 
-        dout = 1
-        for layer in reversed(self.layers):
-            dout = layer.backward(dout)
-
-        reversed(self.layers)
+        #reversed(self.layers)
         self.layers[0].update()
         self.layers[2].update()
 
