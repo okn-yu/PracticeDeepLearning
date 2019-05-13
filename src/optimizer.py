cc1afaf4f2207be5b013e6a5519603675da26d9b
okn-yu <mofuneko@okn-yunoMacBook-Air.local>
2019-05-13 23:34:51 +0900

    updated.

---
+++ src/optimizer.py
@@ -0,0 +1,20 @@
+class SGD:
+    # Stochastic Gradient Descent
+
+    def __init__(self, lr):
+        pass
+
+    def update(self, W, dW):
+    self.W -= LEARNING_RATE * self.dW
+    self.b -= LEARNING_RATE * self.db
+
+class Momentum_SGD:
+    # Momentum SDG
+
+    def __init__(self):
+        pass
+
+class AdaGard:
+
+    def __init__(self):
+        pass
